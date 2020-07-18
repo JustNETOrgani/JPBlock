60d24d19ba68d92e2bdb00cd3f17f6624ff4acae
JustNETlab <justnetonline@outlook.com>
2020-07-18 18:40:01 +0800

    Banner images updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -50,6 +50,9 @@ export default {
     this.images.push(
       // 'https://cdn.pixabay.com/photo/2018/03/09/18/46/blockchain-3212312__340.jpg',
       'https://cdn.pixabay.com/photo/2013/07/12/13/23/book-146937__340.png',
+      'https://egret-plus.eu/wp-content/uploads/2019/07/HCSA-Web-12-Publication-banner-1024x321.jpg',
+      'https://www.lawimperial.com/wp-content/uploads/2020/01/slide2.jpg',
+      'https://www.iferp.in/blog/wp-content/uploads/2020/03/blog-publish-paper-1.jpg',
       'https://cdn.pixabay.com/photo/2018/03/31/05/06/blockchain-3277335__340.png',
       'https://cdn.pixabay.com/photo/2020/06/10/09/31/bitcoin-5281988__340.jpg',
       'https://cdn.pixabay.com/photo/2017/01/01/20/26/book-1945459__340.jpg',
@@ -57,9 +60,9 @@ export default {
       // 'https://cdn.pixabay.com/photo/2018/08/05/12/04/blockchain-3585482__340.jpg',
       'https://cdn.pixabay.com/photo/2018/09/07/09/10/ethereum-3660218__340.jpg', // Ethereum Pix.
       'https://cdn.pixabay.com/photo/2013/07/18/14/56/open-163976__340.jpg',
-      'https://cdn.pixabay.com/photo/2015/11/03/09/30/puzzle-1020431__340.jpg',
-      'https://cdn.pixabay.com/photo/2019/10/25/10/49/audit-4576720__340.jpg',
-      'https://cdn.pixabay.com/photo/2018/03/20/18/07/bitcoin-3244107__340.jpg',
+      // 'https://cdn.pixabay.com/photo/2015/11/03/09/30/puzzle-1020431__340.jpg',
+      // 'https://cdn.pixabay.com/photo/2019/10/25/10/49/audit-4576720__340.jpg',
+      // 'https://cdn.pixabay.com/photo/2018/03/20/18/07/bitcoin-3244107__340.jpg',
       'https://cdn.pixabay.com/photo/2018/01/11/21/27/desk-3076954__340.jpg'
     )
     this.startRotation()
